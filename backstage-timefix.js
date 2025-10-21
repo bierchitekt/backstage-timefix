085fa4d540f74e030fabddac05d92dbf77799559
bierchitekt <a-badu@gmx.de>
2025-10-21 22:09:52 +0200

    fix script

--- backstage-timefix.js
+++ backstage-timefix.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name        Backstage munich time fix
+// @namespace   https://backstage.eu
+// @match        https://*/*
 // @description Fixes the broken time on the backstage details page. The entry and start time is not shown correctly normally on mobile devices
 // @author      bierchitekt
 // @update      https://github.com/bierchitekt/backstage-timefix/raw/main/backstage-timefix.js
 // @version     1.0
 // ==/UserScript==
+
+
 (function() {
     document.querySelector("#maincontent > div.columns > div > div.product-info-main > div.page-title-wrapper.product > div > div.row").style.display = "block";
-});
+})();
+
